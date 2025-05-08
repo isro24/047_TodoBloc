ada400a240f07e8619ace5ffd0bcb3141eb7c6ed
isro24 <alamat_email_anda@example.com>
2025-05-08 09:13:30 +0700

    Menambah Text Todo List pada TodoPage

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -7,6 +7,17 @@ class TodoPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final _key = GlobalKey<FormState>();
     final _controller = TextEditingController();
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Text('Todo List')
+              ],
+            ),
+        )
+      ),
+    );
   }
 }
